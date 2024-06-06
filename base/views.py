f8b55e3bb0578c522977a3fdc02ec88bb38c593d
Yousef Bin Madi <yousefjumaan@gmail.com>
2024-06-06 10:04:27 +0100

    login setup

--- base/views.py
+++ base/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
-from django.shortcuts import render
-def home(request):
-    return render(request, 'home.html')
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib import messages
+from django.contrib.auth import login, logout
 
-def rooms(request):
-    return render(request,'rooms.html')
+
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            messages.error(request, 'User does not exist')
+            return render(request, 'base/login_register.html', {'page': page})
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
+
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+def home(request):
+    return render(request, 'base/home.html')
+
